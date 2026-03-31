c23f379bab8e25986c281a459e61303586e8afa0
Mauricio Erazo <antonierazo13@hotmail.com>
2026-03-31 16:18:03 +0200

    Refactor NavBar component to use dynamic links and add IssuePage component

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,29 +1,33 @@
+'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { GiSpottedBug } from 'react-icons/gi';
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues' },
+  ];
+
   return (
     <nav className="flex space-x-6 border-b border-b-zinc-300 mb-5 px-5 h-14 items-center">
       <Link href="/">
         <GiSpottedBug className="text-2xl" />
       </Link>
+
       <ul className="flex space-x-6">
-        <li>
-          <Link
-            href="/"
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
-          >
-            Dashboard
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/issues"
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
-          >
-            Issues
-          </Link>
-        </li>
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className={`${currentPath === link.href ? 'text-zinc-900' : 'text-zinc-500'} hover:text-zinc-800 transition-colors`}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
---
+++ app/issues/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IssuePage = () => {
+  return (
+    <div>Issue Page</div>
+  )
+}
+
+export default IssuePage
