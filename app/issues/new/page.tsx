460ebe13aa93dd23e0b8fd84a0feca5bc66bb1f9
Mauricio Erazo <antonierazo13@hotmail.com>
2026-04-01 21:40:15 +0200

    feat: implement SimpleMDE for issue description and update dependencies

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,14 +1,30 @@
 'use client';
 
-import { Button, TextArea, TextField } from '@radix-ui/themes';
-import Link from 'next/link';
+import { Button, TextField } from '@radix-ui/themes';
+import dynamic from 'next/dynamic';
+import 'easymde/dist/easymde.min.css';
+import type { Options } from 'easymde';
+import { useMemo } from 'react';
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+  ssr: false,
+});
 
 const NewIssuePage = () => {
+  const autofocusNoSpellcheckerOptions = useMemo(() => {
+    return {
+      autofocus: true,
+      spellChecker: false,
+    } as Options;
+  }, []);
+
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Title" radius="large" />
-      <TextArea placeholder="Description" radius="large" />
-
+      <SimpleMDE
+        options={autofocusNoSpellcheckerOptions}
+        placeholder="Description of the issue."
+      />
       <Button size="2" variant="soft">
         Submit New Issue
       </Button>
