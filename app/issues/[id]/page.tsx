415d8b7bb17d2886ec4e286475fd27aba4712d0e
Mauricio Erazo <antonierazo13@hotmail.com>
2026-04-04 18:53:32 +0200

    feat: enhance issue detail page layout with structured components and improved styling

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,5 +1,7 @@
 import prisma from '@/prisma/client';
+import { Card, Flex, Heading } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
+import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 
 interface IssueDetailPageProps {
   params: Promise<{ id: string }>;
@@ -16,10 +18,14 @@ const IssueDetailPage = async ({ params }: IssueDetailPageProps) => {
 
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <Heading>{issue.title}</Heading>
+      <Flex gap="4" my="2" align="center">
+        <IssueStatusBadge status={issue.status} />
+        <p>{issue.createdAt.toDateString()}</p>
+      </Flex>
+      <Card>
+        <p>{issue.description}</p>
+      </Card>
     </div>
   );
 };
