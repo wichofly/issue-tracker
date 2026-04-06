06adb1d13cd6373723753bb7dad6f928610592f7
Mauricio Erazo <antonierazo13@hotmail.com>
2026-04-06 22:49:30 +0200

    feat: replace loading component with IssueFormSkeleton for improved loading experience

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,27 +1,2 @@
-import { Box, Card, Flex, Heading, Skeleton, Text } from '@radix-ui/themes';
-
-const LoadingIssueDetailPage = () => {
-  return (
-    <Box className="max-w-xl">
-      <Heading>
-        <Skeleton height="1rem" />
-      </Heading>
-      <Flex gap="4" my="2" align="center">
-        <Skeleton width="5rem" />
-        <Skeleton width="8rem" />
-      </Flex>
-      <Card className="prose" mt="4">
-        <Skeleton>
-          <Text>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-            iusto tempora maiores distinctio, iste impedit quo accusantium ex,
-            officia est quia perspiciatis eos dolores. Nihil unde deleniti dolor
-            minima perspiciatis.
-          </Text>
-        </Skeleton>
-      </Card>
-    </Box>
-  );
-};
-
-export default LoadingIssueDetailPage;
+import IssueFormSkeleton from '../_components/IssueFormSkeleton';
+export default IssueFormSkeleton;
