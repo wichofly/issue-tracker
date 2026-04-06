122ee61e05e98e5c8a346b0fedf5d053180485c2
Mauricio Erazo <antonierazo13@hotmail.com>
2026-04-06 18:02:26 +0200

    feat: add EditIssueButton component and integrate it into IssueDetailPage

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,8 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import prisma from '@/prisma/client';
-import { Box, Button, Card, Flex, Grid, Heading } from '@radix-ui/themes';
+import { Box, Grid } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
-import ReactMarkDown from 'react-markdown';
-import { Pencil2Icon } from '@radix-ui/react-icons';
-import Link from 'next/link';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
 
 interface IssueDetailPageProps {
   params: Promise<{ id: string }>;
@@ -24,21 +22,11 @@ const IssueDetailPage = async ({ params }: IssueDetailPageProps) => {
   return (
     <Grid columns={{ initial: '1', md: '2' }} gap="5" width="auto">
       <Box>
-        <Heading>{issue.title}</Heading>
-        <Flex gap="4" my="2" align="center">
-          <IssueStatusBadge status={issue.status} />
-          <p>{issue.createdAt.toDateString()}</p>
-        </Flex>
-        <Card className="prose" mt="4">
-          <ReactMarkDown>{issue.description}</ReactMarkDown>
-        </Card>
+        <IssueDetails issue={issue} />
       </Box>
 
       <Box>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-        </Button>
+        <EditIssueButton issueId={issue.id} />
       </Box>
     </Grid>
   );
