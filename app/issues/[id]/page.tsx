3a21f7c525e0f6bef390dd6703db5f9690893f81
Mauricio Erazo <antonierazo13@hotmail.com>
2026-04-06 17:38:14 +0200

    feat: enhance issue detail page with edit button and improved layout

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,10 @@
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import prisma from '@/prisma/client';
-import { Card, Flex, Heading } from '@radix-ui/themes';
+import { Box, Button, Card, Flex, Grid, Heading } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import ReactMarkDown from 'react-markdown';
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import Link from 'next/link';
 
 interface IssueDetailPageProps {
   params: Promise<{ id: string }>;
@@ -20,16 +22,25 @@ const IssueDetailPage = async ({ params }: IssueDetailPageProps) => {
   // await delay(2000); // Simulate loading delay
 
   return (
-    <div>
-      <Heading>{issue.title}</Heading>
-      <Flex gap="4" my="2" align="center">
-        <IssueStatusBadge status={issue.status} />
-        <p>{issue.createdAt.toDateString()}</p>
-      </Flex>
-      <Card className="prose" mt="4">
-        <ReactMarkDown>{issue.description}</ReactMarkDown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: '1', md: '2' }} gap="5" width="auto">
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex gap="4" my="2" align="center">
+          <IssueStatusBadge status={issue.status} />
+          <p>{issue.createdAt.toDateString()}</p>
+        </Flex>
+        <Card className="prose" mt="4">
+          <ReactMarkDown>{issue.description}</ReactMarkDown>
+        </Card>
+      </Box>
+
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
