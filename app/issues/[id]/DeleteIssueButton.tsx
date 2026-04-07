508f6241df109467d6687a14f68d5fdb9f1d99db
Mauricio Erazo <antonierazo13@hotmail.com>
2026-04-07 18:01:25 +0200

    feat: enhance DeleteIssueButton with loading state and spinner during deletion

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { AlertDialog, Button, Flex } from '@radix-ui/themes';
+import { AlertDialog, Button, Flex, Spinner } from '@radix-ui/themes';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
@@ -8,13 +8,16 @@ import { useState } from 'react';
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
 
   const handleDelete = async () => {
     try {
+      setIsDeleting(true);
       await axios.delete(`/api/issues/${issueId}`);
       router.push('/issues');
       router.refresh();
     } catch (error) {
+      setIsDeleting(false);
       setError(true);
     }
   };
@@ -23,7 +26,14 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red">Delete Issue</Button>
+          <Button
+            color="red"
+            disabled={isDeleting}
+            style={{ cursor: isDeleting ? 'not-allowed' : 'pointer' }}
+          >
+            Delete Issue
+            {isDeleting && <Spinner />}
+          </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
           <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
@@ -34,12 +44,17 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
 
           <Flex gap="3" mt="4" justify="end">
             <AlertDialog.Cancel>
-              <Button variant="soft" color="gray">
+              <Button variant="soft" color="gray" style={{ cursor: 'pointer' }}>
                 Cancel
               </Button>
             </AlertDialog.Cancel>
             <AlertDialog.Action>
-              <Button variant="solid" color="red" onClick={handleDelete}>
+              <Button
+                variant="solid"
+                color="red"
+                onClick={handleDelete}
+                style={{ cursor: 'pointer' }}
+              >
                 Delete
               </Button>
             </AlertDialog.Action>
