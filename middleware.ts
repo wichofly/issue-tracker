34a69c540150c9be37ff99d0477fd535ff001427
Mauricio Erazo <antonierazo13@hotmail.com>
2026-04-09 16:21:48 +0200

    fix: correct middleware export and update matcher pattern for issue editing routes

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
-export { default } from 'next-auth/middleware';
+export { default as middleware } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/issues/new', '/issues/:id+/edit'],
+  matcher: ['/issues/new', '/issues/:id/edit'],
 };
