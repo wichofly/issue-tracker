c19f6cee1ac478b52e5b28e92f93aba18bf0b6f8
Mauricio Erazo <antonierazo13@hotmail.com>
2026-04-09 17:00:01 +0200

    feat: implement user fetching in AssigneeSelect and add GET endpoint for users

---
+++ app/api/users/route.ts
@@ -0,0 +1,12 @@
+import prisma from '@/prisma/client';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const GET = async (request: NextRequest) => {
+  const users = await prisma.user.findMany({
+    orderBy: {
+      name: 'asc',
+    },
+  });
+
+  return NextResponse.json(users);
+};
