a0439c108c431e8e389c76dd1c90fbbf837cc9d8
Mauricio Erazo <antonierazo13@hotmail.com>
2026-04-10 20:41:35 +0200

    feat: add IssueStatusFilter component for filtering issues by status

--- app/issues/IssueActions.tsx
+++ app/issues/IssueActions.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@radix-ui/themes';
 import Link from 'next/link';
+import IssueStatusFilter from './IssueStatusFilter';
 
 const IssueActions = () => {
   return (
-    <div className="mb-5">
+    <div className="flex justify-between mb-5">
+      <IssueStatusFilter />
       <Button size="2" variant="soft">
         <Link href="/issues/new">New Issue</Link>
       </Button>
