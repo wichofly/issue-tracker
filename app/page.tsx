0e6b0748afcc44739015fbb6a36783211503a3ac
Mauricio Erazo <antonierazo13@hotmail.com>
2026-04-21 21:09:54 +0200

    feat: add LatestIssues component to display the most recent issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
-import Pagination from './components/Pagination';
+import LatestIssues from './LatestIssues';
 
-type HomeProps = {
-  searchParams: Promise<{ page?: string }>;
-};
-
-export const Home = async ({ searchParams }: HomeProps) => {
-  const { page } = await searchParams;
-  const currentPage = Math.max(1, Number.parseInt(page ?? '', 10) || 1);
-
-  return <Pagination itemCount={100} pageSize={10} currentPage={currentPage} />;
+export const Home = async () => {
+  return <LatestIssues />;
 };
 
 export default Home;
